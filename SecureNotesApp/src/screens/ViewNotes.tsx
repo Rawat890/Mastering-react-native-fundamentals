b80b52e687373cbbeedc2a458a204b89785881cd
Rawat890 <rohanrawat7508@gmail.com>
2025-11-17 10:44:41 +0530

    chore: ViewNotes screen created

---
+++ SecureNotesApp/src/screens/ViewNotes.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ViewNotes() {
+  return (
+    <View>
+      <Text>ViewNotes</Text>
+    </View>
+  )
+}
