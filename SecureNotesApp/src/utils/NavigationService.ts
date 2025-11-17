1b3c022ee32af3eb379e65b5942779127b1fa2d6
Rawat890 <rohanrawat7508@gmail.com>
2025-11-17 12:23:59 +0530

    chore: navigation utility file added

---
+++ SecureNotesApp/src/utils/NavigationService.ts
@@ -0,0 +1,42 @@
+import {
+  createNavigationContainerRef,
+  StackActions,
+  CommonActions,
+} from '@react-navigation/native';
+
+export const navigationRef = createNavigationContainerRef();
+
+export function navigate(name: string, params?: object) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name as never, params as never);
+  }
+}
+
+export function replace(name: string, params?: object) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.replace(name, params));
+  }
+}
+
+export function push(name: string, params?: object) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.push(name, params));
+  }
+}
+
+export function goBack() {
+  if (navigationRef.isReady() && navigationRef.canGoBack()) {
+    navigationRef.goBack();
+  }
+}
+
+export function reset(routeName: string) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{ name: routeName }],
+      })
+    );
+  }
+}
