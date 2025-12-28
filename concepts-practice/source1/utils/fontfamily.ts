bd26090877f39727cd1d43a4d0416c004bb8da86
Rawat890 <rohanrawat7508@gmail.com>
2025-12-28 21:14:54 +0530

    chore: fontFamily and fontNames utility objects created

---
+++ concepts-practice/source1/utils/fontfamily.ts
@@ -0,0 +1,17 @@
+export const fontFamily = {
+  ralewayBold: require('../assets/fonts/Raleway-Bold.ttf'),
+  ralewayLight: require('../assets/fonts/Raleway-Light.ttf'),
+  ralewayItalic: require('../assets/fonts/Raleway-Italic.ttf'),
+  ralewayMedium: require('../assets/fonts/Raleway-Medium.ttf'),
+  ralewayRegular: require('../assets/fonts/Raleway-Regular.ttf'),
+  ralewaySemiBold: require('../assets/fonts/Raleway-SemiBold.ttf'),
+}
+
+export const fontNames = {
+  bold: 'RalewayBold',
+  light: 'RalewayLight',
+  italic: 'RalewayItalic',
+  medium: 'RalewayMedium',
+  regular: 'RalewayRegular',
+  semiBold: 'RalewaySemiBold',
+};
