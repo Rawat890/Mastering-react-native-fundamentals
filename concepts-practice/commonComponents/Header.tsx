c1effba0fd7f548bd01effec93a3903ec11f40d2
Rawat890 <rohanrawat7508@gmail.com>
2025-12-30 15:06:55 +0530

    chore: Header common component created

---
+++ concepts-practice/commonComponents/Header.tsx
@@ -0,0 +1,56 @@
+import { fontNames } from '@/source1/utils/fontfamily';
+import * as React from 'react';
+import { Appbar, useTheme } from 'react-native-paper';
+
+interface HeaderProps {
+  title: string;
+  subtitle?: string;
+
+  showBackAction?: boolean;
+  onBackPress?: () => void;
+
+  rightIcon?: string;
+  onRightIconPress?: () => void;
+
+  elevated?: boolean;
+}
+
+export default function Header({
+  title,
+  subtitle,
+  showBackAction = false,
+  onBackPress,
+  rightIcon,
+  onRightIconPress,
+  elevated = true,
+}: HeaderProps) {
+  const theme = useTheme();
+
+  return (
+    <Appbar.Header elevated={elevated} >
+      {/* Left */}
+      {showBackAction && (
+        <Appbar.BackAction
+          onPress={onBackPress}
+          accessibilityLabel="Go back"
+        />
+      )}
+
+      {/* Title */}
+      <Appbar.Content
+        title={title}
+        subtitle={subtitle}
+        titleStyle={{ fontWeight: '600' , textAlign: 'center', fontFamily: fontNames.semiBold}}
+      />
+
+      {/* Right */}
+      {rightIcon && (
+        <Appbar.Action
+          icon={rightIcon}
+          onPress={onRightIconPress}
+          accessibilityLabel="Header action"
+        />
+      )}
+    </Appbar.Header>
+  );
+}
