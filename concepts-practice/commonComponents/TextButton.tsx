8bbb8180f13070500ad79aeb3e207b63001cd6ac
Rawat890 <rohanrawat7508@gmail.com>
2026-01-01 11:42:19 +0530

    chore: code formatted

--- concepts-practice/commonComponents/TextButton.tsx
+++ concepts-practice/commonComponents/TextButton.tsx
@@ -2,6 +2,7 @@ import { fontNames } from '@/source1/utils/fontfamily';
 import React from 'react';
 import { GestureResponderEvent, StyleSheet, View } from 'react-native';
 import { Button } from 'react-native-paper';
+import Loader from './Loader';
 
 interface TextButtonProps {
   label?: string;
@@ -16,22 +17,37 @@ interface TextButtonProps {
   style?: object;
 }
 
-export default function TextButton({ label, mode, onPress, disabled, onPressIn, onPressOut, textColor, buttonColor, style }: TextButtonProps) {
+export default function TextButton({
+  label,
+  mode,
+  onPress,
+  disabled,
+  textColor,
+  buttonColor,
+  style,
+  loading,
+}: TextButtonProps) {
+
+  if (loading) {
+    return <Loader loading={loading} />;
+  }
+
   return (
-    <View style={{marginTop: 10}}>
-    <Button
-      mode={mode}
-      onPress={onPress}
-      disabled={disabled}
-      style={[{ backgroundColor: buttonColor }, style]}
-      contentStyle={styles.buttonContainer}
-      labelStyle={[styles.labelStyle, { color: textColor }]}
-    >
-      {label}
-    </Button>
+    <View style={{ marginTop: 10 }}>
+      <Button
+        mode={mode}
+        onPress={onPress}
+        disabled={disabled}
+        style={[{ backgroundColor: buttonColor }, style]}
+        contentStyle={styles.buttonContainer}
+        labelStyle={[styles.labelStyle, { color: textColor }]}
+      >
+        {label}
+      </Button>
     </View>
-  )
+  );
 }
+
 
 const styles = StyleSheet.create({
   buttonContainer: {
