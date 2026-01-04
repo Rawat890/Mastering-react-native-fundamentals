33ba178de4a3cba310c0c2163742ccb9406f4919
Rawat890 <rohanrawat7508@gmail.com>
2026-01-04 16:09:28 +0530

    chore: import paths updated

--- concepts-practice/utils/fontfamily.ts
+++ concepts-practice/utils/fontfamily.ts
@@ -1,10 +1,10 @@
 export const fontFamily = {
-  ralewayBold: require('../assets/fonts/Raleway-Bold.ttf'),
-  ralewayLight: require('../assets/fonts/Raleway-Light.ttf'),
-  ralewayItalic: require('../assets/fonts/Raleway-Italic.ttf'),
-  ralewayMedium: require('../assets/fonts/Raleway-Medium.ttf'),
-  ralewayRegular: require('../assets/fonts/Raleway-Regular.ttf'),
-  ralewaySemiBold: require('../assets/fonts/Raleway-SemiBold.ttf'),
+  ralewayBold: require('../source1/assets/fonts/Raleway-Bold.ttf'),
+  ralewayLight: require('../source1/assets/fonts/Raleway-Light.ttf'),
+  ralewayItalic: require('../source1/assets/fonts/Raleway-Italic.ttf'),
+  ralewayMedium: require('../source1/assets/fonts/Raleway-Medium.ttf'),
+  ralewayRegular: require('../source1/assets/fonts/Raleway-Regular.ttf'),
+  ralewaySemiBold: require('../source1/assets/fonts/Raleway-SemiBold.ttf'),
 }
 
 export const fontNames = {
